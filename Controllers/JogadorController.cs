909f19601e9d174cc6837687b5b31096b1e53812
Gean <geantokito@gmail.com>
2025-05-26 17:58:50 -0300

    alterações na controller, e adicionei css e imagens

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -1,4 +1,5 @@
-﻿using EliminIQ_TCC.Config;
+﻿using AspNetCoreGeneratedDocument;
+using EliminIQ_TCC.Config;
 using EliminIQ_TCC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,37 +8,63 @@ namespace EliminIQ_TCC.Controllers
 {
     public class JogadorController : Controller
     {
-      private readonly DbConfig _dbConfig;
-        
+
+        private readonly DbConfig _dbConfig;
+
         public JogadorController(DbConfig dbConfig)
         {
             _dbConfig = dbConfig;
         }
 
+    
 
-        // GET: /Jogador/Create
-        [HttpGet]
-        public IActionResult Create()
-            => View();   // pega Views/Jogador/Create.cshtml
-
-        // POST: /Jogador/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Jogador jogador)
+        public async Task<ActionResult> BuscarPorId(int id)
         {
-            if (!ModelState.IsValid)
-                return View(jogador);
-
-            await _dbConfig.Jogador.AddAsync(jogador);
-            await _dbConfig.SaveChangesAsync();
-
-            // redireciona de volta ao form ou pra outra página
-            return RedirectToAction("Create");
+            var Jogador = await _dbConfig.Jogador.FindAsync(id);
+            return View("Detalhes", Jogador);
         }
 
-        // remova ou transforme este método:
-        // public IActionResult Index() { … }
+        public async Task<ActionResult> Listar()
+        {
+            var Jogadores = await _dbConfig.Jogador.ToListAsync();
+            return View(Jogadores);
+        }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Jogador jogador)
+        {
+            await _dbConfig.Jogador.AddAsync(jogador);
+            await _dbConfig.SaveChangesAsync();
+            return RedirectToAction("Listar");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Atualizar(Jogador jogador)
+        {
+            _dbConfig.Jogador.Update(jogador);
+            await _dbConfig.SaveChangesAsync();
+            return RedirectToRoute("Update");
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Deletar (int id)
+        {
+            var jogador = _dbConfig.Jogador.Find(id);
+            _dbConfig.Jogador.Remove(jogador);
+            await _dbConfig.SaveChangesAsync();
+            return RedirectToRoute("Delete");
+
+        }
 
     }
+
+
+
 }
+
